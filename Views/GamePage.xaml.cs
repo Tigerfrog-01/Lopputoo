871b37357d7602292eddc5f06576a4a087ef2b67
KŽ <kristopherzuravljov005@gmail.com>
2026-06-11 09:32:18 +0300

    I added cactus.png and you can drag it to lane

--- Views/GamePage.xaml.cs
+++ Views/GamePage.xaml.cs
@@ -7,6 +7,22 @@ namespace Lopputoo.Views
             InitializeComponent();
         }
 
+        private void OnCactusDragStarting(object? sender, DragStartingEventArgs e)
+        {
+            e.Data.Properties["Plant"] = "Cactus";
+        }
+
+        private void OnPlantSquareDrop(object? sender, DropEventArgs e)
+        {
+            if (!e.Data.Properties.ContainsKey("Plant"))
+            {
+                return;
+            }
+
+            PlantSquareLabel.IsVisible = false;
+            PlacedCactusImage.IsVisible = true;
+        }
+
         private async void OnBackClicked(object? sender, EventArgs e)
         {
             await Shell.Current.GoToAsync("..");
